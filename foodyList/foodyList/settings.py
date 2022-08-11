b41b6db1279c254ab6ff5ba3dcce6208d49138f3
RC-002 <83537305+RC-002@users.noreply.github.com>
2022-08-12 00:06:46 +0530

    commit static

--- foodyList/foodyList/settings.py
+++ foodyList/foodyList/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'foodyList.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
