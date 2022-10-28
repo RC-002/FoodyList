00feb3e04bd2edc324832e302f8df4e2e84f5d43
RC-002 <83537305+RC-002@users.noreply.github.com>
2022-10-28 23:40:11 +0530

    email configs and forgot password

--- foodyList/accounts/views.py
+++ foodyList/accounts/views.py
@@ -2,11 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from accounts.models import User, UserProfile
-from accounts.utils import detect_user
+from accounts.utils import detect_user, send_verification_email
 from .forms import UserForm
 from vendor.forms import VendorForm
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_decode
 
 #Restrict the vendor from accessing the customer Dashboard
 def check_user_vendor(user):
@@ -37,7 +39,10 @@ def registerUser(request):
             user.set_password(password)
             user.role = User.CUSTOMER
             user.save()
-            messages.error(request, "Your account has been registered successfully!")
+
+            #Send verification mail
+            send_verification_email(request, user)
+            messages.success(request, "Your account has been created! Wait for the confirmation email")
             return redirect('registerUser')
         else:
             print("not valid")
@@ -67,8 +72,12 @@ def registerVendor(request):
             vendor.user = user
             user_profile = UserProfile.objects.get(user = user)
             vendor.user_profile = user_profile
+            #Send verification mail              
             vendor.save()
-            messages.success(request, "Restaurant registered successfully!")
+            mail_subject = 'Verify your account'
+            email_template = 'accounts/emails/account_verification_email.html'
+            send_verification_email(request, user, mail_subject, email_template) 
+            messages.success(request, "Your restaurant account has been created! Wait for the confirmation email")
             return redirect('registerVendor')
         else:
             print("invalid form")
@@ -80,6 +89,22 @@ def registerVendor(request):
         'v_form': vForm,
     }
     return render(request, 'accounts/registerVendor.html', context)
+
+def activate(request, uidb64, token):
+    #activate the user by setting the is_active status to True
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk = uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, "Account verified!")
+    else:
+        messages.error(request, "Invalid activation link")
+    return redirect('myAccount')
+
 
 def login(request):
     if request.user.is_authenticated:
@@ -120,3 +145,50 @@ def customerDashboard(request):
 def VendorDashboard(request):
     return render(request, 'accounts/VendorDashboard.html')
 
+def forgot_password(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        if User.objects.filter(email = email).exists():
+            user = User.objects.get(email__exact = email)
+
+            #send reset password email
+            mail_subject = 'Reset your Password'
+            email_template = 'accounts/emails/reset_password_email.html'
+            send_verification_email(request, user, mail_subject, email_template)        
+            messages.success(request, "Password reset link sent to your email address")
+            return redirect('login')
+        else:
+            messages.error(request, 'Account does not exist')
+            return redirect('forgot_password')
+    return render(request, 'accounts/forgot_password.html')
+
+def reset_password_validate(request, uidb64, token):
+    #validate the user
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk = uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid']  = uid
+        messages.info(request,'Reset your password')
+        return redirect('reset_password')
+    else:
+        messages.error(request, 'This link has expired')
+        return redirect('forgot_password')
+
+def reset_password(request):
+    if request.method == "POST":
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password == confirm_password:
+            pk = request.session.get('uid')
+            user = User.objects.get(pk = pk)
+            user.set_password(password)
+            user.save()
+            messages.success(request, 'Password reset successful')
+            return redirect('login') 
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('reset_password')
+    return render(request, 'accounts/reset_password.html')
--- foodyList/foodyList/settings.py
+++ foodyList/foodyList/settings.py
@@ -143,4 +143,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
+    messages.INFO: 'warning',
 }
+
+#Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
