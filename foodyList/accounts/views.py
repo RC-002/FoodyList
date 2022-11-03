b008c7a4b042ac1f199b9a9551629eff88946da7
RC-002 <83537305+RC-002@users.noreply.github.com>
2022-11-03 22:12:55 +0530

    dashboard and menu details

--- foodyList/accounts/views.py
+++ foodyList/accounts/views.py
@@ -10,6 +10,7 @@ from django.core.exceptions import PermissionDenied
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_decode
 from vendor.models import Vendor
+from django.template.defaultfilters import slugify
 
 #Restrict the vendor from accessing the customer Dashboard
 def check_user_vendor(user):
@@ -73,6 +74,7 @@ def registerVendor(request):
             user.save()
             vendor = vForm.save(commit = False)
             vendor.user = user
+            vendor.vendor_slug = slugify(vForm.cleaned_data['vendor_name'])+'-'+str(user.id)
             user_profile = UserProfile.objects.get(user = user)
             vendor.user_profile = user_profile
             #Send verification mail              
--- foodyList/marketplace/urls.py
+++ foodyList/marketplace/urls.py
@@ -13,18 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 #file imports
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name="homePage"),
-    path('accounts/', include('accounts.urls')),
-    path('vendor/', include('vendor.urls')),
-    path('marketplace/', include('marketplace.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    path('', views.marketplace, name="marketplace"),
+    path('<slug:vendor_slug>/', views.vendor_detail, name="vendor_detail"),
+]
